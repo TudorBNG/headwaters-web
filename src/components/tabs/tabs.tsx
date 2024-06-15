1a8e474fa02b0295e6d286c1bd558cfd05ca8825
Tudor Bungardean <tudorionutbungardean@gmail.com>
2024-06-15 23:24:48 +0300

    repurpose tabs component

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -6,8 +6,11 @@ const Tabs = ({ currentTab, setCurrentTab }) => {
 
     return (
         <div className={"tabs-container"}>
-            <button className={`tab-button ${currentTab === 0 && "tab-button-selected"}`} onClick={() => setCurrentTab(0)}>Upload</button>
-            <button className={`tab-button ${currentTab === 1 && "tab-button-selected"}`} onClick={() => setCurrentTab(1)}>Job List</button>
+            <div>
+                <button className={`tab-button ${currentTab === 0 && "tab-button-selected"}`} onClick={() => setCurrentTab(0)}>Spec Tab</button>
+                {/* <button className={`tab-button ${currentTab === 2 && "tab-button-selected"}`} onClick={() => { }}>Keys Tab</button> */}
+            </div>
+            <button className={`tab-button ${currentTab === 1 && "tab-button-selected"}`} onClick={() => setCurrentTab(1)}>AI Tab</button>
         </div>
     )
 }
