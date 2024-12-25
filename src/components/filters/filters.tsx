668be21e500089e0dcdaf69d1ceeb3c4d6fa82ba
Tudor Bungardean <tudorionutbungardean@gmail.com>
2024-12-25 22:43:25 +0200

    remove unused import

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo } from "react";
 import "./filters.scss";
 
 import importedDivisions from "./../../utils/divisions.json";
